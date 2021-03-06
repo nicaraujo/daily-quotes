1810c291f8254101591268ec48bc311836a29a28
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-06 13:10:38 -0300

    Added logic to generate quote and small enhancements

--- app/src/main/java/com/cursoandroid/dailyquotes/MainActivity.java
+++ app/src/main/java/com/cursoandroid/dailyquotes/MainActivity.java
@@ -3,6 +3,10 @@ package com.cursoandroid.dailyquotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void generateQuote(View view) {
+        int n = new Random().nextInt(4);
+        String[] texts = {
+                "Frase1",
+                "Frase2",
+                "Frase3",
+                "Frase4"
+        };
+        TextView text = findViewById(R.id.textView);
+        text.setText(texts[n]);
     }
 }
